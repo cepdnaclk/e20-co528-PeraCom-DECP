f3e25e2dec4cc2314f2090898261a32f21cbb9fd
Bimsara Janakantha <janakanthaofficial@gmail.com>
2026-02-28 00:21:21 +0530

    Update index.ts

--- services/identity-service/src/index.ts
+++ services/identity-service/src/index.ts
@@ -4,6 +4,7 @@ import { AppModule } from "./app.module.js";
 import { connectProducer } from "@decp/event-bus";
 import { env } from "./config/validateEnv.config.js";
 import { ValidationPipe } from "@nestjs/common/pipes/index.js";
+import { TraceLoggingInterceptor } from "./trace-logging.interceptor.js";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +26,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // 5. The trace logging interceptor
+  app.useGlobalInterceptors(new TraceLoggingInterceptor());
 
   await app.listen(env.NODE_PORT);
 }
