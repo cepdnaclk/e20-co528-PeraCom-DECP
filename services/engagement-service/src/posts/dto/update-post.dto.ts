21149c24e667bdfb17222900838495e6b3182799
Bimsara Janakantha <janakanthaofficial@gmail.com>
2026-03-01 02:17:02 +0530

    refactor(posts): remove MaxLength validation from postId in UpdatePostDto

--- services/engagement-service/src/posts/dto/update-post.dto.ts
+++ services/engagement-service/src/posts/dto/update-post.dto.ts
@@ -3,7 +3,6 @@ import { IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
 export class UpdatePostDto {
   @IsNotEmpty()
   @IsString()
-  @MaxLength(1500)
   postId!: string;
 
   @IsOptional()
